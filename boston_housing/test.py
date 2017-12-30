85b381adfe3795f177bbfc8e5383df269d3d944d
Yasser <yasser.alarifi@gmail.com>
2017-12-30 16:32:44 +0300

    Initial commit

---
+++ boston_housing/test.py
@@ -0,0 +1,8 @@
+# TODO: Import 'make_scorer', 'DecisionTreeRegressor', and 'GridSearchCV'
+def studentReg(ages_train, net_worths_train):
+    from sklearn import linear_model
+
+    reg = linear_model.LinearRegression()
+
+    reg.fit(ages_train, net_worths_train)
+
---
+++ titanic_survival_exploration/test.py
@@ -0,0 +1,5 @@
+import sklearn.metrics as sk
+
+sk.r2_score(y_true=, y_pred=)
+
+sk.
